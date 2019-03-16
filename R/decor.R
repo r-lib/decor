1b8dd24727eba5d60d1aa35c101e2670ae61b18b
Romain Francois <romain@purrple.cat>
2019-03-16 20:17:45 +0100

    + cpp_decorations()

--- R/decor.R
+++ R/decor.R
@@ -14,3 +14,39 @@ cpp_files <- function(pkg = ".") {
   }
 
 }
+
+#' Decorations in a C++ file
+#'
+#' @param pkg A package, see [devtools::as.package()]
+#' @param files C++ files
+#'
+#' @export
+cpp_decorations <- function(pkg = ".", files = cpp_files(pkg = pkg)) {
+
+  cpp_attribute_pattern <-  paste0(
+    "^[[:blank:]]*",                 ## allow for indentation
+    "//[[:blank:]]*",                ## the comment should be started by //, with potential spaces following
+    "\\[\\[",                        ## the opening square brackets
+    "[[:space:]]*(.*?)[[:space:]]*", ## the material within
+    "\\]\\].*$"                      ## closing brackets
+  )
+
+  map_dfr(files, function(file){
+    lines <- read_lines(file)
+
+    line <- str_which(lines, cpp_attribute_pattern)
+    n <- length(line)
+    text <- lines[line]
+    content <- str_replace(text, cpp_attribute_pattern, "\\1")
+
+    name <- str_replace(content, "\\(.*$", "")
+
+    has_args <- str_detect(content, "\\(")
+    args <- map_if(content, has_args, ~{
+      call_args(parse(text = .x)[[1]])
+    })
+
+    tibble(file, line, name, args, content)
+  })
+
+}
--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,8 @@
 #' @import devtools
 #' @import fs
 #' @import rlang
+#' @import stringr
+#' @import readr
+#' @importFrom purrr map map_if map_dfr
+#' @importFrom tibble tibble
 NULL
